3b4973059ca236eef4b0a0822f5645e89436df01
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-12-25 14:53:57 +0500

    FAQS page added

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,28 @@ import "aos/dist/aos.css";
 import './index.css';
 import Body from './Pages/Body/Body';
 import SignUp from "./Pages/Signup/SignUp";
+import Faqs from './Pages/FAQS/FAQS';
+
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import AOS CSS
+
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 3000, // Set default animation duration
+      easing: 'ease-out-back', // Set easing function
+      once: true, // Trigger animation only once
+    });
+  }, []);
+
   
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Body />}>
           <Route path='/signup' element={ <SignUp/>} />
+          <Route path='/faqs' element={<Faqs></Faqs>}></Route>
         </Route>
     </Routes>
     </BrowserRouter>
