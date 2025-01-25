cc74b71797fa9145aca89caf08eda41aa8ae7690
NidaWaheed From Pakistan <109359002+Nida506@users.noreply.github.com>
2025-01-25 19:23:52 +0500

    Update Body.jsx
    initial commit

--- src/Pages/Body/Body.jsx
+++ src/Pages/Body/Body.jsx
@@ -1,15 +1,1 @@
-import { Outlet } from "react-router-dom"
-import Footer from "../../components/Footer/Footer";
-import Navbar from "../../components/Navbar/Navbar";
-function Body() {
-    
-    return (
-        <div>
-            <Navbar />
-            <Outlet/>
-            <Footer/>
-        </div>
-    )
-}
 
-export default Body;
